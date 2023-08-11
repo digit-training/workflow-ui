55fb1ecb9c90305f2dfddba7ce59f13a650c6fcc
prerna-egov <prerna.kumari@egovernments.org>
2023-08-11 11:16:27 +0530

    added dropdown based on array type

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -6,14 +6,79 @@ const Form = ({ config, formData, onFormChange }) => {
         const updatedData = { ...formData, [key]: value };
         onFormChange(updatedData);
       };
+
+      function formatString(str) {
+        return str
+          .split(/(?=[A-Z])|\s|_/)
+          .map((word) => {
+            if (word.length === 0) return ""; // Handle multiple underscores or spaces
+            return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+          })
+          .join(" ");
+      }
+      
     
+      // return (
+      //   <div className="form-container">
+      //     {Object.keys(config).map((key) => (
+      //       <div key={key}>
+      //         <label className="label">{formatString(key)}</label><br></br>
+      //         {config[key] === String ? (
+      //           <input
+      //           className="input-container"
+      //             type="text"
+      //             value={formData[key] || ''}
+      //             onChange={(e) => handleChange(key, e.target.value)}
+      //           />
+      //         ) : config[key] === Boolean ? (
+      //           <div>
+      //             <label>
+      //               <input
+      //               className="input-container"
+      //                 type="radio"
+      //                 value="true"
+      //                 checked={formData[key] === true}
+      //                 onChange={() => handleChange(key, true)}
+      //               />
+      //               True
+      //             </label>
+      //             <label>
+      //               <input
+      //               className="input-container"
+      //                 type="radio"
+      //                 value="false"
+      //                 checked={formData[key] === false}
+      //                 onChange={() => handleChange(key, false)}
+      //               />
+      //               False
+      //             </label>
+      //           </div>
+      //         ) : null}
+      //       </div>
+      //     ))}
+      //   </div>
+      // );
       return (
         <div className="form-container">
           {Object.keys(config).map((key) => (
             <div key={key}>
-              <label>{key}</label><br></br>
-              {config[key] === String ? (
+              <label className="label">{formatString(key)}</label><br />
+              {Array.isArray(config[key]) ? (
+                <select
+                  className="input-container"
+                  value={formData[key] || ''}
+                  onChange={(e) => handleChange(key, e.target.value)}
+                >
+                  <option value="">Select an option</option>
+                  {config[key].map((option) => (
+                    <option key={option.action} value={option.nextState}>
+                      {option.action}
+                    </option>
+                  ))}
+                </select>
+              ) : config[key] === String ? (
                 <input
+                  className="input-container"
                   type="text"
                   value={formData[key] || ''}
                   onChange={(e) => handleChange(key, e.target.value)}
@@ -22,6 +87,7 @@ const Form = ({ config, formData, onFormChange }) => {
                 <div>
                   <label>
                     <input
+                      className="input-container"
                       type="radio"
                       value="true"
                       checked={formData[key] === true}
@@ -31,6 +97,7 @@ const Form = ({ config, formData, onFormChange }) => {
                   </label>
                   <label>
                     <input
+                      className="input-container"
                       type="radio"
                       value="false"
                       checked={formData[key] === false}
@@ -44,6 +111,8 @@ const Form = ({ config, formData, onFormChange }) => {
           ))}
         </div>
       );
+      
+    
     };
 
     export default Form;
