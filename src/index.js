a91bba5837b788a9eecbe778324a44279ee6489d
prerna-egov <prerna.kumari@egovernments.org>
2023-08-17 16:51:08 +0530

    added Dropdown checkbox component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import DropdownCheckbox from "./components/MultiDropDown";
+import YourFormComponent from "./components/MultiDropDown/Test";
+import DropdownCheckBox from "./components/MultiDropDown";
 // import Popup from "./components/Popup"
 // import { state } from "./constants/demoConfigs/state";
 // import { role } from "./constants/demoConfigs/roles";
@@ -9,6 +12,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    {/* <DropdownCheckbox/> */}
+    {/* <YourFormComponent/> */}
+    {/* <MultiSelectDropdown/> */}
     {/* <Popup config={role}/> */}
   </React.StrictMode>
 );
